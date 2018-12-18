8948bb1ed1815cc01b6279850eb1ee2b0ab40263
Richmond Wong <richmondwong@gmail.com>
2018-12-18 22:03:28 +0000

    First commital

---
+++ navy_schema_revised.sql
@@ -0,0 +1,35 @@
+DROP SCHEMA public cascade;
+CREATE SCHEMA public;
+
+
+CREATE TABLE fleets (
+  id BIGSERIAL PRIMARY KEY);
+
+CREATE TABLE sailors (
+  sailor_name TEXT,
+  dob TIMESTAMP with time zone,
+  id BIGSERIAL PRIMARY KEY
+  );
+
+CREATE TABLE ranks (
+  id BIGSERIAL PRIMARY KEY,
+  title TEXT
+  );
+
+CREATE TABLE assignments (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT,
+  start_date TIMESTAMP with time zone,
+  end_date TIMESTAMP with time zone,
+  rank_id BIGSERIAL REFERENCES ranks(id),
+  sailor_id BIGSERIAL REFERENCES sailors(id)
+  );
+
+CREATE TABLE ships (
+  id BIGSERIAL PRIMARY KEY,
+  assignment_id BIGSERIAL REFERENCES assignments(id),
+  ship_name TEXT,
+  build_date TEXT,
+  fleet_id BIGSERIAL REFERENCES fleets(id)
+);
+
